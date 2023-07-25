3f57656c8300e896495acdf33e80795a7a652952
KlimovRoman <111612276+KlimovRoman@users.noreply.github.com>
2023-07-25 21:43:06 +0300

    Update EntityNotFoundException.java

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/exception/EntityNotFoundException.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/exception/EntityNotFoundException.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.exception;
 public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException() {
     }
+
     public EntityNotFoundException(String msg) {
         super(msg);
     }
