f96b3256e5da1f8f883b772b60ad33bfc7b85f07
KlimovRoman <111612276+KlimovRoman@users.noreply.github.com>
2023-07-27 21:47:33 +0300

    Update FilmController.java

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -22,10 +22,9 @@ public class FilmController {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @PostMapping
-    public void addFilm(@Valid @RequestBody Film filmToAdd){
+    public void addFilm(@Valid @RequestBody Film filmToAdd) {
 
-        if(nameIsEmptyValid(filmToAdd) && descLen200Valid(filmToAdd) &&
-                                    releaseDateValid(filmToAdd) && durationValid(filmToAdd)){
+        if (releaseDateValid(filmToAdd)) {
             filmIdCounter++;
             Film newFilm = new Film(filmIdCounter);
             newFilm.setReleaseDate(filmToAdd.getReleaseDate());
@@ -33,81 +32,51 @@ public class FilmController {
             newFilm.setDescription(filmToAdd.getDescription());
             newFilm.setDuration(filmToAdd.getDuration());
             films.put(filmIdCounter, newFilm);
-            log.info("Добавлен фильм с id=" + filmIdCounter);
+            log.info("Добавлен фильм с id = {}", filmIdCounter);
         } else {
-            log.error("Валидация не пройдена при добавлени фильма");
+            log.info("Валидация не пройдена при добавлени фильма");
             throw new ValidationException("Не пройден один валидаторов");
         }
     }
 
     @PutMapping
-    public void updFilm(@Valid @RequestBody Film filmToUpd){
+    public void updFilm(@Valid @RequestBody Film filmToUpd) {
 
-        if(nameIsEmptyValid(filmToUpd) && descLen200Valid(filmToUpd) &&
-                releaseDateValid(filmToUpd) && durationValid(filmToUpd)){
+        if (releaseDateValid(filmToUpd)) {
 
             final Integer id = filmToUpd.getId();
-            if (films.containsKey(id)){
+            if (films.containsKey(id)) {
                 Film filmFromHash = films.get(id);
                 filmFromHash.setDescription(filmToUpd.getDescription());
                 filmFromHash.setName(filmToUpd.getName());
                 filmFromHash.setDuration(filmToUpd.getDuration());
                 filmFromHash.setReleaseDate(filmToUpd.getReleaseDate());
-                log.info("Обновлен фильм с id=" + id);
+                log.info("Обновлен фильм с id = {}", id);
             } else {
-                log.error("Фильм не найден");
+                log.info("Фильм не найден");
                 throw new EntityNotFoundException("Фильм не найден");
             }
         } else {
-            log.error("Валидация не пройдена при обновлении фильма");
+            log.info("Валидация не пройдена при обновлении фильма");
             throw new ValidationException("Не пройден один валидаторов");
         }
 
     }
 
     @GetMapping
-    public List<Film> getFilms(){
+    public List<Film> getFilms() {
         return new ArrayList<>(films.values());
     }
 
     // методы для валидации
-
-    private boolean nameIsEmptyValid ( Film filmToCheck ){
-        if (filmToCheck.getName()!=null && !filmToCheck.getName().isBlank() && !filmToCheck.getName().isEmpty()){
-            return true;
-        }else {
-            log.error("Валидация не пройдена, имя пустое");
-            return false;
-        }
-    }
-
-    private boolean descLen200Valid ( Film filmToCheck ){
-        if (filmToCheck.getDescription().length() <= 200){
-            return true;
-        } else{
-            log.error("Валидация не пройдена, описание более 200 символов");
-            return false;
-        }
-    }
-
-    private boolean releaseDateValid ( Film filmToCheck ){
+    private boolean releaseDateValid(Film filmToCheck) {
         LocalDate dateToCheck = LocalDate.parse(filmToCheck.getReleaseDate(),formatter);
         LocalDate dateForCompare =  LocalDate.parse("1895-12-28",formatter);
-        if(dateToCheck.isAfter(dateForCompare)){
+        if (dateToCheck.isAfter(dateForCompare)) {
             return true;
         } else {
             log.error("Валидация не пройдена, дата релиза должна быть после 1895-12-28");
             return false;
         }
     }
-
-    private boolean durationValid ( Film filmToCheck){
-        if (filmToCheck.getDuration() > 0){
-            return true;
-        } else {
-            log.error("Валидация не пройдена,длительность фильма должна быть >0");
-            return false;
-        }
-    }
-
 }
