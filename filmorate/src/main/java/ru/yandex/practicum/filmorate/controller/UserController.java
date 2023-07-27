2ed72ac6bc1258bbd38e72646232ff3d14bafb7b
KlimovRoman <111612276+KlimovRoman@users.noreply.github.com>
2023-07-27 21:48:00 +0300

    Update UserController.java

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,11 +3,10 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.EntityNotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
+
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,97 +22,52 @@ public class UserController {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @PostMapping
-    public void addUser (@Valid @RequestBody User userToAdd){
-        if(emailValid(userToAdd) && loginValid(userToAdd) &&
-                        birthValid(userToAdd)) {
-            userIdCounter++;
-            User newUser = new User(userIdCounter);
-            if(userToAdd.getName() == null || userToAdd.getName().equals("null") || userToAdd.getName().isEmpty() ||
-                                                      userToAdd.getName().isBlank()){
-                newUser.setName(userToAdd.getLogin());
-            } else {
-                newUser.setName(userToAdd.getName());
-            }
-            newUser.setLogin(userToAdd.getLogin());
-            newUser.setEmail(userToAdd.getEmail());
-            newUser.setBirthday(userToAdd.getBirthday());
-            users.put(userIdCounter, newUser);
-            log.info("Добавлен юзер с id=" + userIdCounter);
+    public void addUser(@Valid @RequestBody User userToAdd) {
+        userIdCounter++;
+        User newUser = new User(userIdCounter);
+        if (!nameValidation(userToAdd)) {
+            newUser.setName(userToAdd.getLogin());
         } else {
-            log.error("Ошибка валидации юзер не создан");
-            throw new ValidationException("Не пройден один валидаторов");
+            newUser.setName(userToAdd.getName());
         }
-
+        newUser.setLogin(userToAdd.getLogin());
+        newUser.setEmail(userToAdd.getEmail());
+        newUser.setBirthday(userToAdd.getBirthday());
+        users.put(userIdCounter, newUser);
+        log.info("Добавлен юзер с id = {}", userIdCounter);
     }
 
     @PutMapping
-    public void updUser (@Valid @RequestBody User userToUpd){
+    public void updUser(@Valid @RequestBody User userToUpd) {
 
-        if(emailValid(userToUpd) && loginValid(userToUpd) &&
-                                birthValid(userToUpd)) {
-
-            final int id = userToUpd.getId();
-            if(users.containsKey(id)){
-                User userFromHash = users.get(id);
-                userFromHash.setBirthday(userToUpd.getBirthday());
-                if(userToUpd.getName() == null || userToUpd.getName().equals("null") ||
-                        userToUpd.getName().isEmpty() || userToUpd.getName().isBlank()){
-                    userFromHash.setName(userToUpd.getLogin());
-                } else {
-                    userFromHash.setName(userToUpd.getName());
-                }
-                userFromHash.setLogin(userToUpd.getLogin());
-                userFromHash.setEmail(userToUpd.getEmail());
-                log.info("Обновлен юзер с id=" + id);
+        final int id = userToUpd.getId();
+        if (users.containsKey(id)) {
+            User userFromHash = users.get(id);
+            userFromHash.setBirthday(userToUpd.getBirthday());
+            if (!nameValidation(userToUpd)) {
+                userFromHash.setName(userToUpd.getLogin());
             } else {
-                log.error("пользователь не найден!");
-                throw new EntityNotFoundException("пользователь не найден!");
+                userFromHash.setName(userToUpd.getName());
             }
+            userFromHash.setLogin(userToUpd.getLogin());
+            userFromHash.setEmail(userToUpd.getEmail());
+            log.info("Обновлен юзер с id = {}", id);
         } else {
-            log.error("Ошибка валидации юзер не обновлен");
-            throw new ValidationException("Не пройден один валидаторов");
+            log.info("пользователь не найден!");
+            throw new EntityNotFoundException("пользователь не найден!");
         }
     }
 
     @GetMapping
-    public List<User> getAllUsers (){
-    return new ArrayList<>(users.values());
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
     }
 
-
-    // методы для валидации
-
-    private boolean emailValid(User user){
-        if (user.getEmail()!=null && !user.getEmail().isBlank() &&
-                !user.getEmail().isEmpty() && user.getEmail().contains("@")){
-            return true;
-        }else {
-            log.error("Валидация не пройдена, email неверный");
+    private boolean nameValidation(User usr) {
+        if (usr.getName() == null || usr.getName().isBlank()) {
             return false;
-        }
-
-    }
-
-
-    private boolean loginValid (User user){
-        if (user.getLogin()!=null && !user.getLogin().isBlank() &&
-                !user.getLogin().isEmpty() && !user.getLogin().contains(" ")){
-            return true;
-        }else {
-            log.error("Валидация не пройдена, login пустой");
-            return false;
-        }
-    }
-
-    private boolean birthValid (User user) {
-        LocalDate dateToCheck = LocalDate.parse(user.getBirthday(),formatter);
-        LocalDate dateForCompare =  LocalDate.now();
-        if(dateToCheck.isBefore(dateForCompare)){
-            return true;
         } else {
-            log.error("Валидация не пройдена, неверная дата рождения");
-            return false;
+            return true;
         }
     }
-
 }
