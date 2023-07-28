35eff14cba859575ce5a1aae03362a7c7bbb7056
KlimovRoman <111612276+KlimovRoman@users.noreply.github.com>
2023-07-28 11:34:28 +0300

    Update FilmControllerTest.java

--- filmorate/src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ filmorate/src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -12,37 +12,4 @@ import java.net.http.HttpResponse;
 @SpringBootTest
 public class FilmControllerTest {
 
-    @Test
-    void nameIsEmptyValidtest() throws IOException, InterruptedException {
-        String testFilm = "{\n" +
-                "  \"name\": \"nisi eiusmod\",\n" +
-                "  \"description\": \"adipisicing\",\n" +
-                "  \"releaseDate\": \"1967-03-25\",\n" +
-                "  \"duration\": 100\n" +
-                "}";
-        URI uri =  URI.create("http://localhost:8080/films");
-        HttpRequest requestToApi = HttpRequest.newBuilder()
-                .POST(HttpRequest.BodyPublishers.ofString(testFilm))
-                .uri(uri)
-                .build();
-        HttpClient client = HttpClient.newHttpClient();   // HTTP-клиент с настройками по умолчанию
-        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();  // получаем стандартный обработчик тела запроса с конвертацией содержимого в строку
-        HttpResponse<String> response = client.send(requestToApi, handler);  // дергаем метод отправки клиента передав объект запроса и обработчик тела
-
-    }
-
-    @Test
-    void descLen200ValidTest() {
-
-    }
-
-    @Test
-    void releaseDateValidTest() {
-
-    }
-
-    @Test
-    void durationValidTest() {
-
-    }
 }
