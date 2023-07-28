f5f9b4e3bf475a895a6b07d2df4d0510a41dc2af
KlimovRoman <111612276+KlimovRoman@users.noreply.github.com>
2023-07-28 11:43:44 +0300

    Update FilmControllerTest.java

--- filmorate/src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ filmorate/src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -3,12 +3,6 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
 @SpringBootTest
 public class FilmControllerTest {
 
