7168d42513a167ba4ff1520dacb2a21a3f112b40
KlimovRoman <111612276+KlimovRoman@users.noreply.github.com>
2023-08-02 00:34:12 +0300

    Update FilmControllerTest.java

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,11 +13,13 @@ public class FilmControllerTest {
 
     @Test
     public void dateValidMinTest() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date =  LocalDate.parse("1895-12-27",formatter);
         Film film = new Film();
         film.setName("name");
         film.setDuration(10.00);
         film.setDescription("des");
-        film.setReleaseDate("1895-12-27");
+        film.setReleaseDate(date);
         FilmController controller = new FilmController();
         final RuntimeException excep = assertThrows(RuntimeException.class, () -> controller.addFilm(film));
         assertEquals("Не пройдена валидация", excep.getMessage());
@@ -23,11 +27,13 @@ public class FilmControllerTest {
 
     @Test
     public void dateValidEqualTest() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date =  LocalDate.parse("1895-12-28",formatter);
         Film film = new Film();
         film.setName("name");
         film.setDuration(10.00);
         film.setDescription("des");
-        film.setReleaseDate("1895-12-28");
+        film.setReleaseDate(date);
         FilmController controller = new FilmController();
         final RuntimeException excep = assertThrows(RuntimeException.class, () -> controller.addFilm(film));
         assertEquals("Не пройдена валидация", excep.getMessage());
@@ -35,11 +41,13 @@ public class FilmControllerTest {
 
     @Test
     public void dateValidMaxTest() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date =  LocalDate.parse("2005-12-28",formatter);
         Film film = new Film();
         film.setName("name");
         film.setDuration(10.00);
         film.setDescription("des");
-        film.setReleaseDate("2005-12-28");
+        film.setReleaseDate(date);
         FilmController controller = new FilmController();
         controller.addFilm(film);
         List<Film> list = controller.getFilms();
