bbf0588eb211ff97c8aefd6f13945d7f7f3c6592
klimov.roman.vit <klimov.roman.vit@yandex.ru>
2023-08-13 20:11:11 +0300

    10 TZ review 1.12it

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,13 +1,15 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
 
 public interface FilmStorage {
     public Film addFilm(Film filmToAdd);
+
     public Film updFilm(Film filmToUpd);
+
     public List<Film> getFilms();
+
     public Film getFilmById(int id);
 }
