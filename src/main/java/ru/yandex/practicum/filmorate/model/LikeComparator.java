c1a218fc37d282e033156ef55d38eda5155c26e3
klimov.roman.vit <klimov.roman.vit@yandex.ru>
2023-08-13 19:57:32 +0300

    10 TZ review 1.6it

--- src/main/java/ru/yandex/practicum/filmorate/model/LikeComparator.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/LikeComparator.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import java.util.Comparator;
 
-public class LikeComparator implements Comparator<Film>  {
+public class LikeComparator implements Comparator<Film> {
     @Override
     public int compare(Film o1, Film o2) {
         if(o1.getLikesCount() < o2.getLikesCount()) {
