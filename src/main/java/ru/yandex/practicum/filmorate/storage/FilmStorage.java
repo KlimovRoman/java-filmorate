be43f890424c3488c02720138d60a39a7edc74cc
A.Kistanov <aleksey.kistanov@yandex.ru>
2023-09-04 01:37:44 +0500

    fix: Delete conflicts

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import java.util.Optional;
 
 public interface FilmStorage {
+    public List<Film> getFilmsByDirectors(int directorId, String sortBy);
+
     public List<Film> getFilmsBySearch(String query, String[] by);
 
     public Film addFilm(Film filmToAdd);
@@ -18,15 +20,15 @@ public interface FilmStorage {
 
     public void addLike(int filmId, int userLikeId);
 
-    List<Film> getTopMostLikedFilms(int topCount);
+    public void delLike(int filmId, int userLikeId);
+
+    public List<Film> getTopMostLikedFilms(int topCount);
+
+    public List<Film> getCommonFilms(int userId, int friendId);
+
+    public void delFilmById(int filmId);
 
     Map<Integer, List<Integer>> getAllLikedFilms();
 
     List<Film> getRecommendedFilms(List<Integer> recommendedFilmsId);
-
-    public List<Film> getCommonFilms(int userId, int friendId);
-
-    public List<Film> getFilmsBySearch(String fullSort);
-
-    public void delFilmById(int filmId);
 }
