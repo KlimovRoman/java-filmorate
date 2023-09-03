b445015eb443f8f861799af33f752639dc4260b9
Ruslagon <ruslan003.g@yandex.ru>
2023-09-03 17:44:40 +0300

    fix: пустой коммит

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/FilmDbStorage.java
@@ -190,7 +190,7 @@ public class FilmDbStorage implements FilmStorage {
                 " f.duration, r.name_rating, r.mpa_id, count(l.user_id) as total_likes from films" +
                 " as f left join rating r on f.rating_id = r.mpa_id " +
                 "left join likes as l on f.id = l.film_id " +
-                "where " + fullSort + " group by f.id order by total_likes desc;";
+                "where " + fullSort + " group by f.id order by total_likes desc";
         return jdbcTemplate.query(sql, (rs, rowNum) -> makeFilm(rs));
     }
 }
