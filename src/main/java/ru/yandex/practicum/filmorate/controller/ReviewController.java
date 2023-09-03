6c3326881856d9cfccd1522320d71bd792ca2b7b
ProgrammingEnjoyerrr <122672015+ProgrammingEnjoyerrr@users.noreply.github.com>
2023-09-03 15:49:32 +0300

    Add reviews (#11)
    * feat: implement review task
    
    * refactor: add sql constraints, simplify db access
    
    * chore: refactoring sql query
    
    * chore: refactoring sql query, part 2
    
    * chore: refactoring sql query, part 3
    
    * refactor: review_id -> id in db
    
    * refactor: empty space
    
    * refactor: replace filmId with Optional
    
    * refactor: use -> useful
    
    * refactor: like to review db storage

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -0,0 +1,66 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.service.ReviewService;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/reviews")
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService service;
+
+    @PostMapping
+    public Review create(@Valid @RequestBody Review review) {
+        return service.create(review);
+    }
+
+    @PutMapping
+    public Review update(@Valid @RequestBody Review review) {
+        return service.update(review);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        service.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public Review findById(@PathVariable Integer id) {
+        return service.findById(id);
+    }
+
+    @GetMapping()
+    public List<Review> findAll(@RequestParam @Positive Optional<Integer> filmId,
+                                @RequestParam(defaultValue = "10", required = false) @Positive Integer count) {
+        return service.findAll(filmId, count);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void createLike(@PathVariable Integer id, @PathVariable Integer userId) {
+        service.createLike(id, userId);
+    }
+
+    @PutMapping("/{id}/dislike/{userId}")
+    public void createDislike(@PathVariable Integer id, @PathVariable Integer userId) {
+        service.createDislike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void deleteLike(@PathVariable Integer id, @PathVariable Integer userId) {
+        service.deleteLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/dislike/{userId}")
+    public void deleteDislike(@PathVariable Integer id, @PathVariable Integer userId) {
+        service.deleteDislike(id, userId);
+    }
+
+}
